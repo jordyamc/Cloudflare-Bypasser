6a070a78f5290cc6d04a4d6987cf766faa26a01d
Jordy Mendoza <jordyamc@hotmail.com>
2021-05-22 21:25:20 -0500

    Transform into BottonSheet when there is no refresh

--- app/src/main/java/knf/tools/bypass/MainActivity.kt
+++ app/src/main/java/knf/tools/bypass/MainActivity.kt
@@ -2,25 +2,33 @@ package knf.tools.bypass
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val prefs: SharedPreferences by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         testButton.setOnClickListener {
-            startBypass(666, "https://www3.animeflv.net/", showReload = false, maxTryCount = 3, reloadOnCaptcha = true, dialogStyle = 1)
+            startBypass(666, "https://www3.animeflv.net/", lastUA = prefs.getString("ua",null),showReload = false, maxTryCount = 3, reloadOnCaptcha = true, clearCookiesAtStart = true, useDialog = true, dialogStyle = 1)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 666 && resultCode == Activity.RESULT_OK) {
+            prefs.edit {
+                putString("ua",data?.getStringExtra("user_agent"))
+            }
             Log.e(
                 "Bypass created",
                 "\nCookies: ${data?.getStringExtra("cookies")}\nUser agent: ${data?.getStringExtra("user_agent")}".also {
